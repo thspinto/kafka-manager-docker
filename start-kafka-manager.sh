d738940eb3dbf202e78ad5686c7c4bcda233b804
Thiago <thiago.pinto@quintoandar.com.br>
2017-08-11 12:11:17 -0300

    Initialization script fix

--- start-kafka-manager.sh
+++ start-kafka-manager.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ $KM_USERNAME != ''  && $KM_PASSWORD != '' ]]; then
-    sed -i.bak '/^basicAuthentication/d' /kafka-manager-${KM_VERSION}/conf/application.conf
-    echo 'basicAuthentication.enabled=true' >> /kafka-manager-${KM_VERSION}/conf/application.conf
-    echo "basicAuthentication.username=${KM_USERNAME}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-    echo "basicAuthentication.password=${KM_PASSWORD}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-    echo 'basicAuthentication.realm="Kafka-Manager"' >> /kafka-manager-${KM_VERSION}/conf/application.conf
+    sed -i.bak '/^basicAuthentication/d' ${KM_CONFIGFILE} 
+    echo 'basicAuthentication.enabled=true' >> ${KM_CONFIGFILE} 
+    echo "basicAuthentication.username=${KM_USERNAME}" >> ${KM_CONFIGFILE}
+    echo "basicAuthentication.password=${KM_PASSWORD}" >> ${KM_CONFIGFILE}
+    echo 'basicAuthentication.realm="Kafka-Manager"' >> ${KM_CONFIGFILE} 
 fi
 
 exec kafka-manager -Dconfig.file=${KM_CONFIGFILE} "${KM_ARGS}" "${@}"
